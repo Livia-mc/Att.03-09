5b5fd18fed317182ff97f3012415791612984eec
Livia <146363895+Livia-mc@users.noreply.github.com>
2025-09-03 16:18:49 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,21 @@
+  const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+  console.log(numeroAleatorio);
+verif
+  alert(n)
+while(n!=numeroAleatorio, resposta==true){
+
+if(n<numeroAleatorio){
+    alert("Seu chute é muito baixo")
+    n=parseInt(prompt("Qual é seu novo chute? "))
+}
+
+else{
+    alert("Seu número é muito alto")
+    n=parseInt(prompt("Qual é seu novo chute? "))
+    }
+
+
+}
+        alert("Parabéns!!!!! Vc acertou, é o número "+numeroAleatorio)
+
+}
